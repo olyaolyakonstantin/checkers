aeedcc0360a185e01bffb340791420011d5487de
Alex A. Naanou <alex.nanou@gmail.com>
2024-07-29 23:46:16 +0300

    added text checkers field...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ var field = {
 }
 
 var setup = function()  {
-    field.cells = document.querySelectorAll('td')
+	field.cells = document.querySelectorAll('td')
 	field.height = document.querySelectorAll('tr').length
 	field.cellCount = field.cells.length
 	field.width = field.cellCount / field.height
+
+	field.cells.item().classList.add('green')
+
+	var some_text = "abcd"
+	console.log(some_text[1]) // -> b
+	for(var i=0; i < some_field.length; i++){
+		console.log(some_field[i])
+	}
 }
 
-field.cells.item().classList.add('green')
-
-
-var some_text = "abcd"
-console.log(some_text[1]) // -> b
-for(var i=0; i < some_field.length; i++){
-    console.log(some_field[i])
-}
