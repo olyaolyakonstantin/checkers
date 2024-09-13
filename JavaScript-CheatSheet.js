1abbea855b4b22072fe655f30fecf9a7256b3181
Alex A. Naanou <alex.nanou@gmail.com>
2024-09-13 09:32:22 +0300

    fixed style + added js cheatsheet
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

---
+++ JavaScript-CheatSheet.js
@@ -0,0 +1,25 @@
+// Smicolons:
+
+// for counter loop...
+for(var i=0; i < 5; i++){
+	console.log('i:', i)
+}
+
+
+
+// If a line starts with an array literal or priority braces, put a 
+// semicolon before it
+var i = 5
+
+;[1,2,3]
+	.map(function(e){ 
+		return e * 2 })
+
+//
+console.log(123)
+
+;(1 + 2) * 4
+
+
+
+
--- checkers.js
+++ checkers.js
@@ -36,30 +36,77 @@ for(var c of field){
 //*/
 
 
-Wpos = [82, 90, 98, 106, 152, 168, 246, 316];
-var Bpos = [];
-for (var v in field) {
-  if (v == 148) continue; //avoids the 'B' line-name
-  if (field[v] == 'B') Bpos.push(v);
-  if (field[v] == 'W') Wpos.push(v);
+
+var Wpos = [
+	82, 
+	90, 
+	98, 
+	106, 
+	152, 
+	168, 
+	246, 
+	316,
+]
+var Bpos = []
+for(var v in field){
+	//avoids the 'B' line-name
+	if(v == 148){ 
+		continue 
+	}
+	if(field[v] == 'B'){ 
+		Bpos.push(v)
+	}
+	if(field[v] == 'W'){
+		Wpos.push(v)
+	}
 }
 // Possible simple steps fro W_player
-var Wsteps = 0;
-var Bsteps = 0;
-for (v of Wpos) {
-  if ((v-4) % 74 != 0 && field[v+70] == ' ') Wsteps++; //left border
-  if ((v-32) % 74 != 0 && field[v+78] == ' ') Wsteps++; //right border
-    if (field[v-70] == 'B' && field[v-140] == ' ') Bteps++;
-    if (field[v-78] == 'B' && field[v-156] == ' ') Bsteps++;
+var Wsteps = 0
+var Bsteps = 0
+for(v of Wpos){
+	//left border
+	if((v-4) % 74 != 0 
+			&& field[v+70] == ' '){ 
+		Wsteps++ 
+	} 
+	//right border
+	if((v-32) % 74 != 0 
+			&& field[v+78] == ' '){
+		Wsteps++ 
+	}
+
+	if(field[v-70] == 'B' 
+			&& field[v-140] == ' '){
+		Bteps++
+	}
+	if(field[v-78] == 'B' 
+			&& field[v-156] == ' '){
+		Bsteps++
+	}
 }
 console.log('possible steps for W player: ', Wsteps )
 
 // Possible simple steps fro B_player
 for (v of Bpos) {
-  if ((v-4) % 74 != 0 && field[v-70] == ' ') Bsteps++;
-  if ((v-32) % 74 != 0 && field[v-78] == ' ') Bsteps++;
-  if (field[v-70] == 'W' && field[v-140] == ' ') Bteps++;
-  if (field[v-78] == 'W' && field[v-156] == ' ') Bsteps++;
+	if((v-4) % 74 != 0 
+			&& field[v-70] == ' '){
+		Bsteps++
+	}
+
+	if((v-32) % 74 != 0 
+			&& field[v-78] == ' '){
+		Bsteps++
+	}
+	if(field[v-70] == 'W' 
+			&& field[v-140] == ' '){
+		Bteps++
+	}
+
+	// in some cases we can use logical operations instead of if statements...
+	field[v-78] == 'W' 
+		&& field[v-156] == ' '
+		&& Bsteps++
+
 }
 console.log('possible steps for B player: ', Bsteps )
 
