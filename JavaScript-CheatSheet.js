176320ffa842f194335e68b290e089e32c60cf87
Alex A. Naanou <alex.nanou@gmail.com>
2024-09-14 09:40:21 +0300

    docs...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- JavaScript-CheatSheet.js
+++ JavaScript-CheatSheet.js
@@ -25,6 +25,30 @@ console.log(123)
 
 
 
+// Null values
+// -----------
+
+// Undefined value
+// - used to represent empty or undefined values
+//
+// NOTE: do not use undefined to check if an array position is empty, 
+// 		use the i in L operation
+undefined
+
+// Not a Number
+// - NaN is a number
+// - returned if an arethmetic peration failes
+// - NaN is not equal to anything (incl. itself)
+//
+// NOTE: to check of a value is NaN us function isNaN(value)
+NaN
+
+// Null
+//  - a basic null value to be used manually
+null
+
+
+
 // FUnctions
 // ---------
 //
