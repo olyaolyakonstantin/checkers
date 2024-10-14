83e7dcdc209f70e53ea87f22dc80a41ff3f29988
Alex A. Naanou <alex.nanou@gmail.com>
2024-10-14 10:16:38 +0300

    new task...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

---
+++ roman.js
@@ -0,0 +1,9 @@
+
+// Write the following functions:
+// 	roman2dec('XLII')
+// 		-> 42
+// 	dec2roman(1337)
+// 		-> 'MCCCXXXVII'
+//
+// References:
+// 	https://en.wikipedia.org/wiki/Roman_numerals
